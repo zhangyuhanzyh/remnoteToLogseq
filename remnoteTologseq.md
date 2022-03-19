6279d2d2df9732eb21bec7626561fed55d693ec5
zhangyuhanzyh <yuhanzhangee@163.com>
2022-03-19 22:23:52 +0800

    folder -- page

--- remnoteTologseq.md
+++ remnoteTologseq.md
@@ -1,3 +1,7 @@
+---
+
+---
+
 ## Remnote json 格式
 
 {"key":[结点名称],"_id":"结点唯一标识编号"，"parent":"父节点编号"，"children":["子节点1编号","子节点2编号","子节点3编号","子节点1编号","子节点4编号","子节点5编号"]}
@@ -10,7 +14,7 @@
 
 **节点存储：**每个节点都是一个字典，有'key'--字符串 'owner'--字符串   'children'--列表 ‘parent’  字符串    **'_id'** 字符串  等关键字
 
-
+- [ ] remnote文件夹page下会有两个多余的block:在用到这些信息的时候判断是否在节点字典中，不在的话删除即可
 
 
 
@@ -22,9 +26,16 @@ title:""
 
 children:[]
 
+文件夹：
+
+```json
+"forceIsFolder": true,
+"folderOpen": false
+```
 
 
 
+***
 
 ## 流程
 
@@ -56,6 +67,76 @@ children:[]
 
 ***
 
-## problem
+## convert
 
-1. rem.json中节点的文本内容是按照list的格式存储的，list中特殊文本例如markdown格式会按照字典的形式存储
+**根据不同数据信息在remnote和logseq存储方式的不同进行转换**
+
+1. ~~rem.json中节点的文本内容是按照list的格式存储的，list中特殊文本例如markdown格式会按照字典的形式存储~~在key中
+
+   - markdown+highlight
+
+     ```json
+     {
+     	"text": "加粗",
+     	"b": true,
+     	"i": "m"
+     },
+     	"  ",
+     {
+     	"text": "颜色",
+     	"h": 4,
+     	"i": "m"
+     },
+     斜体(l)   下划线(u)   高亮(h)
+     ```
+
+     
+
+   - latex
+
+     ```json
+     {
+     	"type": "latex",
+     	"block": false,
+     	"x": true,
+     	"slateIdentifier": 0.25839282510572037,
+     	"text": "W_{T}",
+     	"i": "m"
+     },
+     ```
+
+     
+
+2. **笔记中的双向链接问题**    remnote中引用的标识是    ~~remnote中引用的时候会复制一份信息在json中~~
+
+   *在remnote中块引用和文章引用写法一致*  ：
+
+   ```json
+   添加一个文章字典？（以_id为键值）
+   在无文件夹的情况下："parent": null,
+   ```
+
+   ```json
+   {
+   	"_id": "指向块的id",
+   	"i": "q"
+   },
+   ```
+
+   - **底部有冗余的信息可删除**         
+
+   ​                                                  
+
+3. 记忆卡片问题
+
+4. 
+
+
+
+***
+
+## 注意
+
+#### 在remnote中，一次只能需要导出一个文件夹 
+
+当某笔记中有其他笔记的引用的时候需要按照文件夹导出
--- remnoteTologseq.py
+++ remnoteTologseq.py
@@ -13,7 +13,7 @@ import operator
 以字典的形式存储
 获取主要的笔记信息，在docs中
 '''
-with open('rem1.json','r',encoding='utf-8') as file:
+with open('rem3.json','r',encoding='utf-8') as file:
     remjson_dict = json.load(file)
 #print(remjson_dict)
 
@@ -24,26 +24,48 @@ docs = remjson_dict['docs']  #list 876 元素是dict
 从docs存储的各节点中获取需要的笔记内容及其他信息
 以_id为键值存在节点字典中
 '''
+flag = {}
 dicts = {}
+#判断是page或folder或block
+def judge(id):
+    global dicts
+    print('judge:',id)
+    if "forceIsFolder" in dicts[id]:
+        flag[id] = "folder"
+        return
+    #此处如果不以文件夹为单位导出，第一个就没有键值
+    if flag[ dicts[id]['parent'] ] == "folder":
+        flag[id] = "page"
+        return
+    flag[id] = 'block'
+
+
 for doc in docs:
     if not doc['_id'] in dicts:
         _id = doc['_id']
+        flag[_id] = False #线默认不是page或folder
         key = doc['key']  #列表，存储的是节点内容
         #删除特殊的无用的节点
-        if operator.eq(key,['Document']) | operator.eq(key,['Status'])| operator.eq(key,[{'i': 'q', '_id': 'WWor6M9ZxtgsAw69z'}])| operator.eq(key,['Draft']) :
+        if operator.eq(key,['Document']) | operator.eq(key,['Status'])| operator.eq(key,[{'i': 'q', '_id': 'WWor6M9ZxtgsAw69z'}])| operator.eq(key,['Draft']) | operator.eq(key,[{'i': 'q', '_id': 'xeu4icH3N4Drt3JSR'}]) :
+            continue
+        children = doc['children']  # 列表，存储的是该节点的子节点_id
+        if (len(key) == 0 | len(children) ==0 ):#remnote中最后会有空节点
             continue
         #key_str = '' #列表内容拼接为字符串
         #keyStr = [str(i) for i in key]
         #print(key_str.join(keyStr))
-        children = doc['children']  #列表，存储的是该节点的子节点_id
-        parent = doc['parent']  # 字符串，存储的是节点的父节点_id
-        dicts[doc['_id']] = {'key': key, 'children': children, 'parent': parent}
+        #parent = doc['parent']  # 字符串，存储的是节点的父节点_id
+        #dicts[doc['_id']] = {'key': key, 'children': children, 'parent': parent}
         #可能存在没有parent属性的情况
-        # if 'parent' in doc:
-        #     parent = doc['parent']  #字符串，存储的是节点的父节点_id
-        #     dicts[doc['_id']] = {'key':key,'children':children,'parent':parent}
-        # else:
-        #     dicts[doc['_id']] = {'key': key, 'children': children}
+        if 'parent' in doc:
+            parent = doc['parent']  #字符串，存储的是节点的父节点_id
+            dicts[doc['_id']] = {'key':key,'children':children,'parent':parent}
+        else:
+            dicts[doc['_id']] = {'key': key, 'children': children}
+        if "forceIsFolder" in doc:
+            dicts[_id]['forceIsFolder'] = doc['forceIsFolder']
+        #将节点存入到节点字典中后判断该节点类型
+        judge(_id)
 #打印字典键值及内容
 #print(len(dicts))
 for key,value in dicts.items():
@@ -56,56 +78,97 @@ for key,value in dicts.items():
 '''
 #logseq的json格式
 lsq_dict = {}
-title = list(dicts.values())[0]['key'][0]
-print(title)
-title_id = list(dicts.keys())[0]
-print(title_id)
-root_flag = True
+folderr = list( dicts.values() )[0]['key'][0]
+print('folder:',folderr)
+folderr_id = list(dicts.keys())[0]
+print('folderr_id:',folderr_id)
+folder_flag = True
 '''
-以笔记标题节点为根节点，将所有节点存储到lsq_dict中
+floder 节点 创建一个page   --- 内容是当前folder下面的所有page的链接
+lsq_list 中包含多个 lsq_dict (folder 中 包含多个 page)
+每个lsq_dict对应一个page 存储
+以每个page为根节点，将所有节点存储到一个lsq_dict中
 用一个递归函数创造节点
 '''
-def create_node(child_id):
+def create_folder(pages):
+    global folderr_id
+    page = {}
+    page['title'] = dicts[folderr_id]['key'][0] #folder名字
+    page['children'] = []
+    for page_id in pages:
+        if page_id in dicts.keys():
+            block = {}
+            block["string"] = '[[' + dicts[page_id]['key'][0]#page 引用
+            block["children"] = []
+            page['children'].append(block)
+#返回一个page(dict)
+def create_page(page_id):
+    global dicts
+    page = {}
+    page['title'] = dicts[page_id]['key'][0] #title
+    page['children'] = [] #子block
+    #当前page有 子块
+    childs_id = dicts[page_id]['children']
+    for child_id in childs_id:
+        if child_id in dicts.keys():
+            page['children'].append(create_node(child_id))
+    return page
+def create_node(_id):
     global dicts
     node = {}
-    #print(child_id)
+    #print(page)
     node['string'] = ''
-    content = dicts[child_id]['key']#list
+    content = dicts[_id]['key']#list
     #识别文本内容中的markdown内容 list元素只有两种情况 ：一个字符串  或  一个及以上 字典
     for num in content:
         if isinstance(num,str) & len(content) == 1:
-            node['string'] = num
+            node['string'] += num   ### 注意可能会被修改
         elif isinstance(num,dict):
-            print("markdown文本")
+            #print("特殊格式")
+            print(num)
             #四种情况  b,l.u,i
             if 'b' in num:#加粗
                 node['string'] =node['string'] + '**' + num['text'] + '**' + ' '
             elif 'l' in num:#斜体
-                node['string'] = node['string'] + '***' + num['text'] + '***' + ' '
+                node['string'] = node['string'] + '*' + num['text'] + '*' + ' '
             elif 'u' in num:#下划线
                 node['string'] = node['string'] + '<u>' + num['text'] + '</u>' + ' '
-            else:#高亮
+            elif 'h' in num:#高亮
                 node['string'] = node['string'] + '<highlight>' + num['text'] + '</highlight>' + ' '
-            print(node['string'])
+            elif 'type' in num :#latex
+                node['string']  = node['string'] + '$' + num['text'] + '$' + ' '
+            elif num['i'] == "q":#引用
+                if (flag[num['_id']] == 'folder') | (flag[num['_id']] == 'page'): #该引用是floder或page
+                    node['string'] = node['string'] + '[[' + dicts[num['_id']]['key'][0] + ']]'
+                else :#块引用
+                    print('块引用')
+                    node['string'] = node['string'] + '((' + dicts[num['_id']]['key'][0] + '))'
+    print('最终内容：',node['string'])
     children = []
-    if dicts[child_id]['children']:
-        for childId in dicts[child_id]['children']:
+    if dicts[_id]['children']:
+        for childId in dicts[_id]['children']:
             children.append(create_node(childId))
     node['children'] = children
     return node
 
-lsq_dict['title'] = title
-lsq_dict['children'] = []
-first_childs = dicts[title_id]['children']
-if first_childs:#笔记有节点的话添加到dict['children']中
-    for child_id in first_childs:
-        if child_id in dicts.keys():
-            lsq_dict['children'].append(create_node(child_id))
+lsq_list = []
+#lsq_dict['folderr'] = folderr
+#lsq_dict['children'] = []
+pages = dicts[folderr_id]['children'] #list ---存储各page的_id
+if pages:#folder下面有page
+    folder_page = create_folder(pages)#返回一个folder_page(dict)
+    lsq_list.append(folder_page)
+if pages:#有笔记page，创造page，返回一个dict
+    for page in pages:
+        if page in dicts.keys():#有的话创建一个笔记page（dict）
+            print('page:',page)
+            lsq_list.append(create_page(page))
+            #lsq_dict['children'].append(create_node(page))
 
-print(lsq_dict)
+print(lsq_list)
 
 #logseq_roam.json可以识别的json都是[]中
-lsq_list = [lsq_dict]
+
 
 '''
 保存到result.json文件中
